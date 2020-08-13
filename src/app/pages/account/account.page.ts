3c76fac85f46772b7c62ffffb6ec1b419d9141cb
sskriblo <a.skriblo@gmail.com>
2020-08-13 15:18:19 +0300

    CHAPTER 7 works well

--- src/app/pages/account/account.page.ts
+++ src/app/pages/account/account.page.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import { AuthService, User } from '../../services/auth.service';
+
+import { Plugins, CameraResultType } from '@capacitor/core';
+import { LoadingController } from '@ionic/angular';
+const { Camera } = Plugins;
 
 @Component({
   selector: 'app-account',
@@ -8,14 +13,44 @@ import { Router } from '@angular/router';
   styleUrls: ['./account.page.scss'],
 })
 export class AccountPage implements OnInit {
+  user: User = null;
+  dummyAvatar = '';
 
-  constructor(private afAuth: AngularFireAuth, private router: Router) { }
+  constructor(
+    private afAuth: AngularFireAuth,
+    private router: Router,
+    private authService: AuthService,
+    private loading: LoadingController
+  ) {}
 
   ngOnInit() {
+    this.authService.getUserData().subscribe((user) => {
+      this.user = user;
+      if (!this.user.photoURL) {
+        const name = user.displayName.split(' ').join('+');
+        this.dummyAvatar = `https://eu.ui-avatars.com/api/?name=${name}&background=3dc2ff&color=fff`;
+      }
+    });
   }
 
   async signOut() {
     await this.afAuth.signOut();
     this.router.navigate(['/']);
   }
+
+  async changeAvatar() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Base64,
+    });
+
+    const loading = await this.loading.create();
+    await loading.present();
+
+    this.authService.uploadAvatar(image.base64String).subscribe((res) => {
+      loading.dismiss();
+    });
+  }
 }
+
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+} from '@angular/fire/firestore';
 import { Plugins } from '@capacitor/core';
 const { SignInWithApple } = Plugins;
 import '@codetrix-studio/capacitor-google-auth';
@@ -10,6 +13,13 @@ import 'firebase/auth';
 
 import { AlertController } from '@ionic/angular';
 
+import {
+  AngularFireStorage,
+  AngularFireUploadTask,
+} from '@angular/fire/storage';
+import { switchMap } from 'rxjs/operators';
+import { of, from } from 'rxjs';
+
 export interface User {
   uid: string;
   email: string;
@@ -18,11 +28,15 @@ export interface User {
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  constructor(private afAuth: AngularFireAuth, private afs: AngularFirestore, private alertController: AlertController) { }
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore,
+    private alertController: AlertController,
+    private storage: AngularFireStorage
+  ) {}
 
   // Start the Firebase register process
   async emailSignup({ email, password, fullname }): Promise<any> {
@@ -36,7 +50,6 @@ export class AuthService {
 
   // Sets user data to firestore on login
   private updateUserData(user: User, name = null, image = null): Promise<any> {
-
     const userRef: AngularFirestoreDocument<User> = this.afs.doc(
       `users/${user.uid}`
     );
@@ -54,7 +67,7 @@ export class AuthService {
 
     if (!data.photoURL) {
       delete data.photoURL;
-    }    
+    }
 
     return userRef.set(data, { merge: true });
   }
@@ -77,38 +90,39 @@ export class AuthService {
   }
 
   async appleSignin() {
-    return SignInWithApple.Authorize().then(async res => {
-      if (res.response && res.response.identityToken) {
-        // this.auth.createFirebaseuser(res.response);
-        const appleResponse = res.response;
-        
-        // Create a custom OAuth provider    
-        const provider = new auth.OAuthProvider('apple.com');
+    return SignInWithApple.Authorize()
+      .then(async (res) => {
+        if (res.response && res.response.identityToken) {
+          // this.auth.createFirebaseuser(res.response);
+          const appleResponse = res.response;
 
-        // Create sign in credentials with our token
-        const credential = provider.credential({
-          idToken: appleResponse.identityToken
-        });
+          // Create a custom OAuth provider
+          const provider = new auth.OAuthProvider('apple.com');
 
-        // Call the sign in with our created credentials
-        const afUser = await this.afAuth.signInWithCredential(credential);
+          // Create sign in credentials with our token
+          const credential = provider.credential({
+            idToken: appleResponse.identityToken,
+          });
 
-        let name = '';
-        if (appleResponse.givenName && appleResponse.familyName) {
-          name = `${appleResponse.givenName} ${appleResponse.familyName}`;
+          // Call the sign in with our created credentials
+          const afUser = await this.afAuth.signInWithCredential(credential);
+
+          let name = '';
+          if (appleResponse.givenName && appleResponse.familyName) {
+            name = `${appleResponse.givenName} ${appleResponse.familyName}`;
+          }
+          // Update the user document in Firestore
+          return this.updateUserData(afUser.user, name);
+        } else {
+          this.presentAlert('Error', `Couldn't sign in with Apple!`);
         }
-        // Update the user document in Firestore
-        return this.updateUserData(afUser.user, name);
-
-      } else {
-        this.presentAlert('Error', `Couldn't sign in with Apple!`);
-      }
-    }).catch(err => {
-      // no permission was given
-    });
+      })
+      .catch((err) => {
+        // no permission was given
+      });
   }
 
-  signIn({email, password}) {
+  signIn({ email, password }) {
     return this.afAuth.signInWithEmailAndPassword(email, password);
   }
 
@@ -120,12 +134,41 @@ export class AuthService {
     const alert = await this.alertController.create({
       header,
       message,
-      buttons: ['OK']
+      buttons: ['OK'],
     });
     await alert.present();
   }
 
+  // User functions
+  getUserId() {
+    return auth().currentUser.uid;
+  }
+
+  getUserData() {
+    return this.afs.doc<User>(`users/${this.getUserId()}`).valueChanges();
+  }
+
+  uploadAvatar(base64String) {
+    const filePath = `${this.getUserId()}/avatar`;
+    const fileRef = this.storage.ref(filePath);
+    const task: AngularFireUploadTask = fileRef.putString(
+      base64String,
+      'base64',
+      { contentType: 'image/png' }
+    );
+
+    return from(task).pipe(
+      switchMap((result) => {
+        // Upload Task finished, get URL to the image
+        return fileRef.getDownloadURL();
+      }),
+      switchMap((photoURL) => {
+        // Set the URL to the user document
+        const uploadPromise = this.afs
+          .doc(`users/${this.getUserId()}`)
+          .set({ photoURL }, { merge: true });
+        return from(uploadPromise);
+      })
+    );
+  }
 }
-
-
-
